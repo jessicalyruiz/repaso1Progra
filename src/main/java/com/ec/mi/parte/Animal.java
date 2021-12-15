0dcc1810b356ef15aeb433a6604b9f98e0104d9c
edison <edison@10.1.145.188>
2021-12-14 19:50:42 -0500

    Esta es mi parte del trabajo que lo subo al repositorio central

---
+++ src/main/java/com/ec/mi/parte/Animal.java
@@ -0,0 +1,10 @@
+package com.ec.mi.parte;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Trabajo colaborativo");
+	}
+
+}
